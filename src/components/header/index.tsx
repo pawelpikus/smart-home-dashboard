5ccdeb0cfb7fe264a06f2d5907522b1933890f55
godspeed7 <pawelpikus7@gmail.com>
2022-02-09 18:22:20 +0100

    desktop layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import { Props } from "../../types";
 const Header = ({ children, ...restProps }: Props) => {
   return (
     <div
-      className="fixed bottom-0 left-0 right-0 p-4 bg-white rounded-lg lg:relative lg:col-span-1 lg:h-screen"
+      className="fixed bottom-0 left-0 right-0 justify-center p-4 bg-white rounded-lg lg:flex lg:flex-col lg:rounded-none lg:items-end lg:p-0 lg:relative lg:col-span-1 lg:h-screen"
       {...restProps}
     >
       {children}
@@ -20,7 +20,7 @@ Header.Nav = function HeaderNav({ children, ...restProps }: Props) {
 };
 
 Header.NavItem = function HeaderNavItem({ children }: Props) {
-  return <li>{children}</li>;
+  return <li className="lg:mb-10">{children}</li>;
 };
 
 export default Header;
--- src/containers/HeaderContainer.tsx
+++ src/containers/HeaderContainer.tsx
@@ -5,36 +5,33 @@ import { RiDashboardFill } from "react-icons/ri";
 import { VscGraphLine } from "react-icons/vsc";
 import { AiFillSetting } from "react-icons/ai";
 import ButtonLink from "../components/buttons/ButtonLink";
-import { IconContext } from "react-icons";
 
 const HeaderContainer = () => {
   return (
-    <IconContext.Provider value={{ color: "white" }}>
-      <Header>
-        <Header.Nav>
-          <Header.NavItem>
-            <ButtonLink href="#">
-              <AiFillHome />
-            </ButtonLink>
-          </Header.NavItem>
-          <Header.NavItem>
-            <ButtonLink href="#">
-              <RiDashboardFill />
-            </ButtonLink>
-          </Header.NavItem>
-          <Header.NavItem>
-            <ButtonLink href="#">
-              <VscGraphLine />
-            </ButtonLink>
-          </Header.NavItem>
-          <Header.NavItem>
-            <ButtonLink href="#">
-              <AiFillSetting />
-            </ButtonLink>
-          </Header.NavItem>
-        </Header.Nav>
-      </Header>
-    </IconContext.Provider>
+    <Header>
+      <Header.Nav>
+        <Header.NavItem>
+          <ButtonLink href="#">
+            <AiFillHome />
+          </ButtonLink>
+        </Header.NavItem>
+        <Header.NavItem>
+          <ButtonLink href="#">
+            <RiDashboardFill />
+          </ButtonLink>
+        </Header.NavItem>
+        <Header.NavItem>
+          <ButtonLink href="#">
+            <VscGraphLine />
+          </ButtonLink>
+        </Header.NavItem>
+        <Header.NavItem>
+          <ButtonLink href="#">
+            <AiFillSetting />
+          </ButtonLink>
+        </Header.NavItem>
+      </Header.Nav>
+    </Header>
   );
 };
 
